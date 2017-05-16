23639a8906a39b816d5cefe90cd955cdef0aa56b
Ale <alelopez.utnfrt@gmail.com>
2017-05-16 01:17:51 -0300

    2 Add Subjects
      -Added Subject model
      -Specialty one to many Subjects
      -Added grade_level enum to specialty
      -Added department enum to subjects
      -Added translations for attributes and enums
      -Modified specialty show view to display subjects
      -Subject validates name and specialty presence

--- db/migrate/20170504031834_create_specialties.rb
+++ db/migrate/20170504031834_create_specialties.rb
@@ -2,7 +2,7 @@ class CreateSpecialties < ActiveRecord::Migration[5.0]
   def change
     create_table :specialties do |t|
       t.string :name
-      t.string :grade_level
+      t.integer :grade_level
 
       t.timestamps
     end
